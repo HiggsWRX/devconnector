bdb278b7a57bd73462962ec8b4e6d82aaf26f3fb
HiggsWRX <hicampos93@gmail.com>
2019-08-06 17:38:53 +0100

    profiles component reducer and routes, get profile, edit profile implemented

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -17,4 +17,39 @@ export const getCurrentProfile = () => async dispatch => {
             payload: { msg: error.response.data.msg, status: error.response.status }
         });
     }
+};
+
+// Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile updated' : 'Profile created', 'success'),);
+
+        if (!edit) {
+            history.push('/dashboard');
+        }
+    } catch (error) {
+        const errors = error.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.data.msg, status: error.response.status }
+        });
+    }
 }
